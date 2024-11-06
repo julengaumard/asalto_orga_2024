871b829057f56f8e323ac7d31142d0f9e791b67a
Julen Gaumard <julengaumard@gmail.com>
2024-11-06 20:45:24 -0300

    Agregado gitingore, implementando el sistema de menu, con una simulacion de switch

--- graphics.asm
+++ graphics.asm
@@ -7,9 +7,10 @@ sub     rsp,8
 call    printf
 add     rsp,8
 %endmacro
-
+ 
 section .data
-    titulo  db ' $$$$$$\                      $$\   $$\                     ', 10
+    titulo  db 10
+            db ' $$$$$$\                      $$\   $$\                     ', 10
             db  '$$  __$$\                     $$ |  $$ |                   ', 10
             db '$$ /  $$ | $$$$$$$\  $$$$$$\  $$ |$$$$$$\    $$$$$$\        ', 10
             db '$$$$$$$$ |$$  _____| \____$$\ $$ |\_$$  _|  $$  __$$\       ', 10
@@ -21,15 +22,41 @@ section .data
 
     menu    db 10
             db '1. Iniciar Juego', 10
-            db '2. Salir', 10
+            db '2. Crear Juego Personalizado', 10
+            db '3. Cargar Partida', 10
+            db '4. Salir', 10
             db 10
             db 'Opcion: ', 0
+
+    inputFormat      db      "%i",0 
+    emsg             db      10,'ERROR: Ingrese un numero valido.',10,10,0 
  
+section .bss
+    opcion resw 1
+
 section .text
     global print_menu 
 
 print_menu:
     printCadena titulo
     printCadena menu
+
+    mov rdi,inputFormat
+    mov rsi,opcion
+    sub     rsp,8
+    call    scanf   
+    add     rsp,8
+    cmp     rax,1
+    jl      error
+    cmp     dword[opcion],4
+    jg      error
+    cmp     dword[opcion],1
+    jl      error
+
+    mov     ah, [opcion]
     ret
  
+error: 
+    mov     al, 0
+    printCadena emsg
+    ret
--- main.asm
+++ main.asm
@@ -1,39 +1,8 @@
 global main
-extern printf  
-extern scanf 
+extern printf 
+extern scanf
  
-
-section .data
-    board   db 32, 32, 88, 88, 88, 32, 32
-            db 32, 32, 88, 88, 88, 32, 32
-            db 88, 88, 88, 88, 88, 88, 88
-            db 88, 88, 88, 88, 88, 88, 88
-            db 88, 88, 95, 95, 95, 88, 88
-            db 32, 32, 95, 95, 79, 32, 32
-            db 32, 32, 79, 95, 95, 32, 32
-
-    format      db  ' %c ',0 
-    formatSalto db  ' %c ',10,0
-
-    textoOpcion db  10,'Turno jugador %i:',10,0
-
-    largo   db 2
-    largo_linea   db 7
-    unidad   db 1
-
-section .bss
-    aux     resq 1
-
-section .text
-    extern print_menu 
-
-main:
-
-    ; sub     rsp,8
-    ; call    print_menu
-    ; add     rsp,8
-
-
+%macro imprimir_tablero 0
     mov ecx, 49
 
 imprimir_tablero:
@@ -48,7 +17,7 @@ imprimir_tablero:
     je hay_salto
 
 evitamos: 
-    mov rdi,format
+    mov rdi,formatoTablero
 continuo_imprimendo: 
     mov rsi,[board+r8]
     ; add sil, 2 ; En principio podria agregar para mostrar el que desee
@@ -62,7 +31,7 @@ continuo_imprimendo:
     loop imprimir_tablero
 
  
-    mov rdi,textoOpcion
+    mov rdi,textoTurnoJuego
     sub     rsp,8
     call    printf
     add     rsp,8
@@ -70,30 +39,57 @@ continuo_imprimendo:
     ret
 
 hay_salto: 
-    mov rdi,formatSalto
+    mov rdi,formatoTableroSalto
     jmp continuo_imprimendo
+%endmacro
 
 
+section .data
+    board   db 32, 32, 88, 88, 88, 32, 32
+            db 32, 32, 88, 88, 88, 32, 32
+            db 88, 88, 88, 88, 88, 88, 88
+            db 88, 88, 88, 88, 88, 88, 88
+            db 88, 88, 95, 95, 95, 88, 88
+            db 32, 32, 95, 95, 79, 32, 32
+            db 32, 32, 79, 95, 95, 32, 32
 
+    formatoTablero      db  ' %c ',0 
+    formatoTableroSalto db  ' %c ',10,0
+    textoTurnoJuego db  10,'Turno jugador %i:',10,0
 
-; db 0, 1, 2, 3, 4, 5, 6
-; db 7, 8, 9, 10, 11, 12, 13
-; db 14, 15, 16, 17, 18, 19, 20
-; db 21, 22, 23, 24, 25, 26, 27
-; db 28, 29, 30, 31, 32, 33, 34
-; db 35, 36, 37, 38, 39, 40, 41
-; db 42, 43, 44, 45, 46, 47, 48
+    largo   db 2
+    largo_linea   db 7
+    unidad   db 1
 
+section .bss
+    aux         resq 1 
+
+section .text
+    extern print_menu 
+
+main:
+
+menu:
+    
+    sub     rsp,8
+    call    print_menu  ; Imprime el menu y procesa el input
+    add     rsp,8
+ 
+    cmp     al, 1       ; Salta al codigo del juego
+    je      game
+
+    ; Hay que agregar el jmp a la carga de partida y para personalizar
+
+    cmp     al, 4       ; Termina la ejecucion
+    je      game
+
+    jmp menu
+
+game:
+    imprimir_tablero
+    ; procesar logica juego
+    jmp game
+
+exit:
+    ret
    
- 
-
-
-
-
-
-
-
-
-
- 
- 
