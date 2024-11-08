1119eea77d6f01af613084ad32b4297e769c855c
Julen Gaumard <julengaumard@gmail.com>
2024-11-08 00:33:50 -0300

    Simplificado y transformado en funcion imprimir_tablero

--- graphics.asm
+++ graphics.asm
@@ -30,12 +30,28 @@ section .data
 
     inputFormat      db      "%i",0 
     emsg             db      10,'ERROR: Ingrese un numero valido.',10,10,0 
+
+    posicion_tablero    db 01, 32, 88, 88, 88, 32, 32
+                        db 32, 32, 88, 88, 88, 32, 32
+                        db 88, 88, 88, 88, 88, 88, 88
+                        db 88, 88, 88, 88, 88, 88, 88
+                        db 88, 88, 95, 95, 95, 88, 88
+                        db 32, 32, 95, 95, 79, 32, 32
+                        db 32, 32, 79, 95, 95, 32, 32
+
+    formato_tablero         db  ' %c ',0 
+    formato_tablero_salto   db  ' %c ',10,0
+    titulo_tablero          db  10,'Tablero:                        Posiciones:',10,0
+    desplaza_tablero        dq  0
+    largo_linea2             db  7
  
 section .bss
     opcion resw 1
+    puntero_tablero resq 1
 
 section .text
     global print_menu 
+    global print_tablero_new
 
 print_menu:
     printCadena titulo
@@ -60,3 +76,40 @@ error:
     mov     al, 0
     printCadena emsg
     ret
+
+print_tablero_new:
+
+    mov     [puntero_tablero], rdi 
+
+    mov     rdi,titulo_tablero
+    sub     rsp,8
+    call    printf
+    add     rsp,8
+ 
+continue_print:
+    
+    mov r15, [puntero_tablero]
+    add r15, [desplaza_tablero]
+    
+    mov     rsi,[r15]
+    mov     rdi,formato_tablero
+
+    mov ax, word[desplaza_tablero] 
+    div byte[largo_linea2]
+    cmp ah, 6
+    je hay_salto
+
+continuo_imprimendo: 
+    sub     rsp,8
+    call    printf
+    add     rsp,8 
+
+    add qword[desplaza_tablero], 1
+    cmp qword[desplaza_tablero], 49
+    jne continue_print
+
+    ret
+
+hay_salto: 
+    mov     rdi,formato_tablero_salto
+    jmp     continuo_imprimendo
--- main.asm
+++ main.asm
@@ -2,46 +2,6 @@ global main
 extern printf 
 extern scanf
  
-%macro imprimir_tablero 0
-    mov ecx, 49
-
-imprimir_tablero:
- 
-    mov r8, 49
-    sub r8, rcx
-    mov [aux], r8
-
-    mov ax, r8w 
-    div byte[largo_linea]
-    cmp ah, 6
-    je hay_salto
-
-evitamos: 
-    mov rdi,formatoTablero
-continuo_imprimendo: 
-    mov rsi,[board+r8]
-    ; add sil, 2 ; En principio podria agregar para mostrar el que desee
-    sub     rsp,8
-    call    printf
-    add     rsp,8
-
-    mov rcx, 49 
-    sub rcx, [aux]
-
-    loop imprimir_tablero
-
-    mov     rdi,textoTurnoJuego
-    sub     rsp,8
-    call    printf
-    add     rsp,8
-
-    jmp proceso_logica
- 
-hay_salto: 
-    mov rdi,formatoTableroSalto
-    jmp continuo_imprimendo
-%endmacro
-
 
 section .data
     board   db 32, 32, 88, 88, 88, 32, 32
@@ -51,22 +11,16 @@ section .data
             db 88, 88, 95, 95, 95, 88, 88
             db 32, 32, 95, 95, 79, 32, 32
             db 32, 32, 79, 95, 95, 32, 32
-
-    formatoTablero      db  ' %c ',0 
-    formatoTableroSalto db  ' %c ',10,0
+ 
     textoTurnoJuego db  10,'Turno jugador %i:',10,0
-
-    largo   db 2
-    largo_linea   db 7
-    unidad   db 1
 
     formatoTurno      db  ' %c ',0 
 
-section .bss
-    aux         resq 1 
+section .bss 
 
 section .text
     extern print_menu 
+    extern print_tablero_new
 
 main:
 
@@ -87,16 +41,18 @@ menu:
     jmp menu
 
 game:
-    imprimir_tablero
-    ; procesar logica juego
-proceso_logica:
-
-    mov rdi, formatoTurno
+    lea rdi, [board]
     sub     rsp,8
-    call    scanf   
+    call    print_tablero_new   
     add     rsp,8
 
-    jmp game
+; proceso_logica:
+;     ; Aca esta la logica del juego
+;     mov rdi, formatoTurno
+;     sub     rsp,8
+;     call    scanf   
+;     add     rsp,8
+;     jmp game
 
 exit:
     ret
