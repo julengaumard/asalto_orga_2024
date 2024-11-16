faf036a2e8721323e304f9c9580ff8d0a7005f55
Sebastian Kupri <seebassnk@gmail.com>
2024-11-16 02:23:09 -0300

    Se agrega una funcionalidad que verifica si un oficial tiene movimientos válidos o no.

--- logic.asm
+++ logic.asm
@@ -22,7 +22,7 @@ section .data
     txtdestino db 'Ingrese la casilla de destino: ',0
     mensaje_verificacion_mov_oficial db 10, "El oficial seleccionado no posee movimientos válidos", 10, 0
     movimiento_valido db 0
-    vector_desplazamientos times 8 db -8, -7, -6, -1, 1, 6, 7, 8 
+    vector_desplazamientos db -8, -7, -6, -1, 1, 6, 7, 8 
 
 section .bss
    
@@ -30,42 +30,49 @@ section .bss
 section .text 
 global validar_movimiento_oficial
 global verificar_salto_y_eliminar_oficial
+global verificar_mov_oficial 
 
+
+; Si el movimiento es posible, establece true en movimiento_valido
 verificar_mov_oficial:
-    mov byte[movimiento_valido], 0                  ; Establece que movimiento_valido es false
-    lea r11, [vector_desplazamientos]               ; Carga el puntero al primer elemento del vector_desplazamiento
-    mov cx, 8                                       ; Carga 8 en CX para utilizar un loop
+    mov byte[movimiento_valido], 0                      ; Establece que movimiento_valido es false
+    lea r11, [vector_desplazamientos]                   ; Carga el puntero al primer elemento del vector_desplazamiento
+    mov cx, 8                                           ; Carga 8 en CX para utilizar un loop
 
 ; Verifica si los alrededores del oficial están libres o si hay un soldado y se puede capturar
 loop_verificar_mov_oficial:
-    mov r10, [ficha_a_mover]                        ; Posición actual de la ficha a mover
-    sub r10, 1                                      ; Se ajusta a 0-index
-    lea r12, [board]                                ; Guarda el puntero al primer elemento del tablero
-    add r12, r10                                    ; El puntero del tablero apunta a la posición de la ficha a mover
+    mov r10, [ficha_a_mover]                            ; Posición actual de la ficha a mover
+    sub r10, 1                                          ; Se ajusta a 0-index
+    lea r12, [board]                                    ; Guarda el puntero al primer elemento del tablero
+    add r12, r10                                        ; El puntero del tablero apunta a la posición de la ficha a mover
 
-    call_function esta_borde_lateral_tablero        ; Verifica si la ficha se encuentra en los bordes laterales del tablero
-    call_function esta_borde_superior_tablero       ; Verifica si la ficha se encuentra en el borde superior del tablero
-    call_function esta_borde_inferior_tablero       ; Verifica si la ficha se encuentra en el borde inferior del tablero
+    call_function esta_borde_lateral_tablero                 ; Verifica si la ficha se encuentra en los bordes laterales del tablero
+    call_function esta_borde_superior_inferior_tablero       ; Verifica si la ficha se encuentra en el borde superior o en el borde inferior del tablero
 
-    add r12, r11                                    ; Calcula el desplazamiento respecto a la posición de la ficha
-    cmp r12, 95                                     ; Verifica si hay un espacio libre
+    mov dl, [r11]
+    movsx rdx, dl
+    add r12, rdx                                        ; Calcula el desplazamiento respecto a la posición de la ficha
+    cmp byte[r12], 95                                   ; Verifica si hay un espacio libre
     je movimiento_posible
 
-    cmp r12, 32                                     ; Verifica si está fuera de los movimientos permitidos pero dentro del tablero (Los espacios en las esquinas del tablero)
+    cmp byte[r12], 32                                   ; Verifica si está fuera de los movimientos permitidos pero dentro del tablero (Los espacios en las esquinas del tablero)
     je continuar_verificacion_mov_oficial
 
+    cmp byte[r12], 79
+    je continuar_verificacion_mov_oficial               ; Verifica si hay otro oficial en la posición
+
     ; (Debería compararse con una variable que contenga la ficha de los soldados)
-    cmp r12, 88                                     ; Verifica si hay un soldado en la posición y si se puede capturar 
+    cmp byte[r12], 88                                   ; Verifica si hay un soldado en la posición y si se puede capturar 
     je captura_posible
 
 continuar_verificacion_mov_oficial:
     cmp byte[movimiento_valido], 1                      ; Un solo movimiento válido es suficiente por lo tanto termina la subrutina
     je finalizar_verificacion_mov_oficial
 
-    inc r11                                         ; Mueve el puntero del vector_desplazamiento a la siguietne posición
-    loop loop_verificar_mov_oficial                 ; Si no encontró un movimiento válido, intenta nuevamente pero con otro desplazamiento
+    inc r11                                             ; Mueve el puntero del vector_desplazamiento a la siguietne posición
+    loop loop_verificar_mov_oficial                     ; Si no encontró un movimiento válido, intenta nuevamente pero con otro desplazamiento
     mov rdi, mensaje_verificacion_mov_oficial
-    call_function puts                              ; Imprime por pantalla que el oficial no tiene movimientos válidos
+    call_function puts                                  ; Imprime por pantalla que el oficial no tiene movimientos válidos
 
 
     ; (Faltaría tener en cuenta, en otra función podría ser, que si ambos oficiales no pueden moverse, el juego termina y ganan los soldados)
@@ -74,94 +81,92 @@ finalizar_verificacion_mov_oficial:
     ret
 
 esta_borde_lateral_tablero:
-    mov ax, r10w                                     ; Copia la posicion del oficial a AX
-    idiv byte[7]                                           
-    cmp ah, 0                                       ; Si el resto es 0, se encuentra en el borde izquierdo del tablero
+    xor rdx, rdx
+    xor rax, rax
+    mov ax, r10w                                    ; Copia la posicion del oficial a AX
+    xor rbx, rbx
+    mov bx, 7
+    idiv bx                                           
+    cmp dx, 0                                       ; Si el resto es 0, se encuentra en el borde izquierdo del tablero
     je verificar_desplazamiento_izquierda
 
-    cmp ah, 6                                       ; Si el resto es 6, se encuentra en el borde derecho del tablero
+    cmp dx, 6                                       ; Si el resto es 6, se encuentra en el borde derecho del tablero
     je verificar_desplazamiento_derecha
 
     ret
 
 verificar_desplazamiento_derecha:
-    cmp r11, -6                                     ; Diagonal superior derecha
+    cmp byte[r11], -6                                   ; Diagonal superior derecha
     je continuar_verificacion_mov_oficial
-    cmp r11, 1                                      ; Posición del medio derecha
+    cmp byte[r11], 1                                    ; Posición del medio derecha
     je continuar_verificacion_mov_oficial
-    cmp r11, 8                                      ; Diagonal inferior derecha
+    cmp byte[r11], 8                                    ; Diagonal inferior derecha
     je continuar_verificacion_mov_oficial
 
     ret
 
 verificar_desplazamiento_izquierda:
-    cmp r11, -8                                     ; Diagonal superior izquierda
+    cmp byte[r11], -8                                   ; Diagonal superior izquierda
     je continuar_verificacion_mov_oficial
-    cmp r11, -1                                     ; Posición del medio izquierda
+    cmp byte[r11], -1                                   ; Posición del medio izquierda
     je continuar_verificacion_mov_oficial
-    cmp r11, 6                                      ; Diagonal inferior izquierda
+    cmp byte[r11], 6                                    ; Diagonal inferior izquierda
     je continuar_verificacion_mov_oficial
 
     ret
 
-esta_borde_superior_tablero:
-    mov rax, r12                                    ; Carga la dirección del tablero con la posición de la ficha en R14
-    sub rax, 6                                      ; Diagonal superior derecha
+esta_borde_superior_inferior_tablero:
+    mov rax, r12                                            ; Carga la dirección del tablero con la posición de la ficha en RAX
+    xor rdx, rdx                                            ; Limpia RDX
+    mov dl, [r11]                                           ; Carga el desplazamiento de la posición almacenada en el vector al que apunta R11
+    movsx rdx, dl
+    add rax, rdx                                            ; Suma a la posición del oficial el desplazamiento
     lea rbx, [board]
-    cmp rax, rbx                                    ; Verifica si la posición del desplazamiento se sale del rango del tablero
-    jl verificar_desplazamiento_arriba
+    cmp rax, rbx                                            ; Verifica si la posición del desplazamiento se sale del rango del tablero
+    jl movimiento_fuera_tablero
+
+    add rbx, 48
+    cmp rax, rbx
+    jg movimiento_fuera_tablero
 
     ret
 
-verificar_desplazamiento_arriba:
-    cmp r11, -8                                     ; Diagonal superior izquierda
-    je continuar_verificacion_mov_oficial
-    cmp r11, -7                                     ; Posición del medio superior
-    je continuar_verificacion_mov_oficial
-    cmp r11, -6                                     ; Diagonal superior derecha
-    je continuar_verificacion_mov_oficial
-
-    ret
-
-esta_borde_inferior_tablero:
-    mov rax, r12                                    ; Carga la dirección del tablero con la posición de la ficha en RAX
-    add rax, 6                                      ; Diagonal inferior izquierda
-    lea rbx, [board]                                ; Carga la dirección del tablero en la posición inicial en RBX
-    add rbx, 48                                     ; Mueve el puntero del tablero cargado en RBX a la posición final
-    cmp rax, rbx                                    ; Verifica si la posición del desplazamiento se sale del rango del tablero
-    jg verificar_desplazamiento_abajo
-
-    ret
-
-verificar_desplazamiento_abajo:
-    cmp r11, 6                                     ; Diagonal inferior izquierda
-    je continuar_verificacion_mov_oficial
-    cmp r11, 7                                     ; Posición del medio inferior
-    je continuar_verificacion_mov_oficial
-    cmp r11, 8                                     ; Diagonal inferior derecha
-    je continuar_verificacion_mov_oficial
-
-    ret
+movimiento_fuera_tablero:
+    jmp continuar_verificacion_mov_oficial
 
 movimiento_posible:
     mov byte[movimiento_valido], 1
     jmp continuar_verificacion_mov_oficial
 
 captura_posible:
-    add r10, r11                                    ; Desplaza la posición cargada en R10 a la posición donde se encuentra el soldado
+    xor rdx, rdx
+    mov dl, [r11]
+    movsx rdx, dl
+    add r10, rdx                                            ; Desplaza la posición cargada en R10 a la posición donde se encuentra el soldado
 
-    call_function esta_borde_lateral_tablero        ; Verifica si la ficha se encuentra en los bordes laterales del tablero
-    call_function esta_borde_superior_tablero       ; Verifica si la ficha se encuentra en el borde superior del tablero
-    call_function esta_borde_inferior_tablero       ; Verifica si la ficha se encuentra en el borde inferior del tablero
+    call_function esta_borde_lateral_tablero                ; Verifica si la ficha se encuentra en los bordes laterales del tablero
+    call_function esta_borde_superior_inferior_tablero      ; Verifica si la ficha se encuentra en el borde superior o en el borde inferior del tablero
+ 
 
     ; En este punto, se verifica que el desplazamiento del salto respecto al soldado está dentro del tablero
-    add r10, r11                                    ; Desplaza la posición cargada en R10 a la posición donde se debe hacer el salto
+    xor rdx, rdx
+    mov dl, [r11]
+    movsx rdx, dl
+    add r10, rdx                                            ; Desplaza la posición cargada en R10 a la posición donde se debe hacer el salto
 
+    lea r12, [board]
+    add r12, r10
     ; (Debería compararse con una variable que contenga la ficha de los soldados)
-    cmp r10, 88                                     ; Verifica si hay un soldado en la posición
+    cmp byte[r12], 88                                       ; Verifica si hay un soldado en la posición
     je continuar_verificacion_mov_oficial
 
+    cmp byte[r12], 32                                       ; Verifica si hay un casillero inválido o " " en la posición
+    je continuar_verificacion_mov_oficial
 
+    cmp byte[r12], 79                                       ; Verifica si hay otro oficial en la posición
+    je continuar_verificacion_mov_oficial               
+
+    ; Si llega hasta este punto, la captura es posible
     ret
 
 
